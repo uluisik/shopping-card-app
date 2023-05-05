c6713f0565bbc55c0ae13373630b9851f4a2c821
ahmet <ahmet55uluisik05@gmail.com>
2023-05-05 23:54:25 +0300

    created Amazonand Card components

--- src/components/amazon/Amazon.js
+++ src/components/amazon/Amazon.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Amazon.css";
+import list from "./AmazonData";
+import Card from "../card/Card";
 const Amazon = () => {
-  return <div>Amazon</div>;
+  return (
+    <section>
+      {list.map((item) => {
+        <Card />;
+      })}
+    </section>
+  );
 };
 
 export default Amazon;
